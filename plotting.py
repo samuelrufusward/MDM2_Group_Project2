27c734ed850aa33ab061dfed520393084cd5da7a
Sam Ward <sam.ward@starhat.co.uk>
2021-03-02 11:34:16 +0000

    final bar graph

--- plotting.py
+++ plotting.py
@@ -2,42 +2,49 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-from run_scenarios import run_all_scenarios
+from run_scenarios import run_all_scenarios, run_model1
 
-wait_times = run_all_scenarios()
 
-model1_wait_times = wait_times[0]
-model2_wait_times = wait_times[1]
-model3_wait_times = wait_times[2]
+def plotbar(): # Plots comparative bar graph of average waiting times for each vehicle for each model
+    wait_times = run_all_scenarios()
 
-all_average1 = sum([model1_wait_times[i]['all'] for i in range(len(model1_wait_times))]) / len(model1_wait_times)
-small_average1 = sum([model1_wait_times[i]['small'] for i in range(len(model1_wait_times))]) / len(model1_wait_times)
-large_average1 = sum([model1_wait_times[i]['large'] for i in range(len(model1_wait_times))]) / len(model1_wait_times)
+    model1_wait_times = wait_times[0]
+    model2_wait_times = wait_times[1]
+    model3_wait_times = wait_times[2]
 
-all_average2 = sum([model2_wait_times[i]['all'] for i in range(len(model2_wait_times))]) / len(model2_wait_times)
-small_average2 = sum([model2_wait_times[i]['small'] for i in range(len(model2_wait_times))]) / len(model2_wait_times)
-large_average2 = sum([model2_wait_times[i]['large'] for i in range(len(model2_wait_times))]) / len(model2_wait_times)
+    all_average1 = sum([model1_wait_times[i]['all'] for i in range(len(model1_wait_times))]) / len(model1_wait_times)
+    small_average1 = sum([model1_wait_times[i]['small'] for i in range(len(model1_wait_times))]) / len(
+        model1_wait_times)
+    large_average1 = sum([model1_wait_times[i]['large'] for i in range(len(model1_wait_times))]) / len(
+        model1_wait_times)
 
-all_average3 = sum([model3_wait_times[i]['all'] for i in range(len(model3_wait_times))]) / len(model3_wait_times)
-small_average3 = sum([model3_wait_times[i]['small'] for i in range(len(model3_wait_times))]) / len(model3_wait_times)
-large_average3 = sum([model3_wait_times[i]['large'] for i in range(len(model3_wait_times))]) / len(model3_wait_times)
+    all_average2 = sum([model2_wait_times[i]['all'] for i in range(len(model2_wait_times))]) / len(model2_wait_times)
+    small_average2 = sum([model2_wait_times[i]['small'] for i in range(len(model2_wait_times))]) / len(
+        model2_wait_times)
+    large_average2 = sum([model2_wait_times[i]['large'] for i in range(len(model2_wait_times))]) / len(
+        model2_wait_times)
 
-print("all1:", all_average1)
-print("small1:", small_average1)
-print("large1:", large_average1)
-print("all2:", all_average2)
-print("small2:", small_average2)
-print("large2:", large_average2)
-print("all3:", all_average3)
-print("small3:", small_average3)
-print("large3:", large_average3)
+    all_average3 = sum([model3_wait_times[i]['all'] for i in range(len(model3_wait_times))]) / len(model3_wait_times)
+    small_average3 = sum([model3_wait_times[i]['small'] for i in range(len(model3_wait_times))]) / len(
+        model3_wait_times)
+    large_average3 = sum([model3_wait_times[i]['large'] for i in range(len(model3_wait_times))]) / len(
+        model3_wait_times)
 
-small_car_averages = [small_average1, small_average2, small_average3]
-large_vehicle_averages = [large_average1, large_average2, large_average3]
-all_vehicle_averages = [all_average1, all_average2, all_average3]
+    print("all1:", all_average1)
+    print("small1:", small_average1)
+    print("large1:", large_average1)
+    print("all2:", all_average2)
+    print("small2:", small_average2)
+    print("large2:", large_average2)
+    print("all3:", all_average3)
+    print("small3:", small_average3)
+    print("large3:", large_average3)
 
-def plot1():
-    labels = ['1', '2', '3']
+    small_car_averages = [small_average1, small_average2, small_average3]
+    large_vehicle_averages = [large_average1, large_average2, large_average3]
+    all_vehicle_averages = [all_average1, all_average2, all_average3]
+
+    labels = ['0', '1', '2']
 
     x = np.arange(len(labels))  # the label locations
     width = 0.25  # the width of the bars
@@ -49,7 +56,8 @@ def plot1():
 
     # Add some text for labels, title and custom x-axis tick labels, etc.
     ax.set_ylabel('Average Waiting Time')
-    ax.set_title('Average Waiting Times For Different Queue Capacities')
+    ax.set_xlabel('Large Vehicle Storage Capacity')
+    ax.set_title('Average Waiting Times For Different Large Vehicle Storage Capacities')
     ax.set_xticks(x)
     ax.set_xticklabels(labels)
     ax.legend()
@@ -57,11 +65,21 @@ def plot1():
 
     plt.show()
 
-def plot2():
+
+def plothistogram(): # Plots histogram
+
+    model1_wait_times = run_model1()
+
+    all_average1 = sum([model1_wait_times[i]['all'] for i in range(len(model1_wait_times))]) / len(model1_wait_times)
+    small_average1 = sum([model1_wait_times[i]['small'] for i in range(len(model1_wait_times))]) / len(
+        model1_wait_times)
+    large_average1 = sum([model1_wait_times[i]['large'] for i in range(len(model1_wait_times))]) / len(
+        model1_wait_times)
+
     fig = plt.figure()
 
-    plt.plot([float(model1_wait_times["all"][i]) for i in range(len(model1_wait_times))])
+    plt.hist([float(model1_wait_times[i]["all"]) for i in range(len(model1_wait_times))])
 
     plt.show()
 
-plot1()
+plotbar()
--- run_scenarios.py
+++ run_scenarios.py
@@ -26,6 +26,32 @@ def run(model):
             return all_events
 
 
+def run_model1():
+    model1_wait_times = []
+
+    for i in range(100):
+        vehicles = []
+
+        tunnel_sections = [
+            TunnelSection('East', (1711, 0), [], []),
+            TunnelSection('West', (0, 0), [], []),
+            TunnelSection('Middle', (800, 0), [], [])
+        ]
+
+        model = Tunnel(tunnel_sections, vehicles, 2, 0)
+
+        events_list = run(model)
+
+        events_dict = get_event_times_dict(events_list)
+
+        average_wait_times_dict = get_average_wait_time_dict(events_dict)
+
+        model1_wait_times.append(average_wait_times_dict)
+
+    print(model1_wait_times)
+    return model1_wait_times
+
+
 def run_all_scenarios():
 
     model1_wait_times = []
